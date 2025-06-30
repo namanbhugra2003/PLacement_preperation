96c662273ce5725912efd7f0d38e2f7d5853f884
Naman Bhugra <namanbhugra7@gmail.com>
2025-06-30 22:38:37 +0530

    Describe your changes here

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // 🔗 MongoDB Connection
-mongoose.connect(MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
