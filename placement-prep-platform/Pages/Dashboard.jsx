d007cc37a70890ad290afb0a1db7f063dfe2e455
Naman Bhugra <namanbhugra7@gmail.com>
2025-07-01 23:49:34 +0530

    Update Dashboard.jsx

--- placement-prep-platform/Pages/Dashboard.jsx
+++ placement-prep-platform/Pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+axios.withCredentials=true;
 
 import Sidebar from "../components/Sidebar";
 import DSA from "../components/dashboard/DSA";
