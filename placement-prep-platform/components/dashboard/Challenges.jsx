79f48fda8800c0da6e50ec202e6929e12a04bdbf
Naman Bhugra <namanbhugra7@gmail.com>
2025-07-01 23:51:56 +0530

    Update Challenges.jsx

--- placement-prep-platform/components/dashboard/Challenges.jsx
+++ placement-prep-platform/components/dashboard/Challenges.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+axios.withCredentials=true;
 
 export default function Challenges() {
   const [todayChallenge, setTodayChallenge] = useState(null);
