5a830b04e1a0ed49e4907289c7ca80a36bfdb1fc
Naman Bhugra <namanbhugra7@gmail.com>
2025-07-02 00:21:49 +0530

    Update Login.jsx

--- placement-prep-platform/Pages/Login.jsx
+++ placement-prep-platform/Pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        `${process.env.REACT_APP_API_URL}/api/auth/login`,
+        `https://get-your-placement.onrender.com/api/auth/login`,
         { email, password },
         { withCredentials: true } // Send cookie to server
       );
