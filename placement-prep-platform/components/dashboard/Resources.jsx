6f026ea3898417404f1424fd5bd4f84dbcb68316
Naman Bhugra <namanbhugra7@gmail.com>
2025-07-01 23:54:05 +0530

    Update Resources.jsx

--- placement-prep-platform/components/dashboard/Resources.jsx
+++ placement-prep-platform/components/dashboard/Resources.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
+axios.defaults.withCredentials = true;
 export default function Resources() {
   const [resources, setResources] = useState([]);
   const [formData, setFormData] = useState({ title: "", link: "", description: "" });
