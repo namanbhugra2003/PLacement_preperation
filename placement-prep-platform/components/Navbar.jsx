ac7c3d3ebe57085b10ef6f367592ef3b6fe77ba7
Naman Bhugra <namanbhugra7@gmail.com>
2025-07-04 02:42:52 +0530

    Update Navbar.jsx

--- placement-prep-platform/components/Navbar.jsx
+++ placement-prep-platform/components/Navbar.jsx
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-900 text-white p-4 shadow-md relative">
-      <div className="container mx-auto flex justify-between items-center relative">
+    <nav className="bg-gray-900 text-white p-4 shadow-md">
+      <div className="container mx-auto flex items-center justify-between relative">
+        
+        {/* Left Spacer (for centering help) */}
+        <div className="w-1/3"></div>
+        
         {/* Centered Logo */}
-        <div className="absolute left-1/2 transform -translate-x-1/2">
-          <Link to="/" className="text-2xl font-bold">PlacementPrep</Link>
+        <div className="w-1/3 text-center">
+          <Link to="/" className="text-3xl font-bold block">Get Your Placement</Link>
         </div>
-
+        
         {/* Navigation Links */}
-        <div className="ml-auto space-x-4">
+        <div className="w-1/3 flex justify-end space-x-4">
           <Link to="/" className="hover:text-blue-400">Home</Link>
           <Link to="/register" className="hover:text-blue-400">Register</Link>
           <Link to="/login" className="hover:text-blue-400">Login</Link>
